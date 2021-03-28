9e64a12b0a909047ffee260778907ad54b6fb4a1
asladej1 <63958414+asladej1@users.noreply.github.com>
2021-03-27 21:09:27 -0500

    Update phrase.py

--- phrase.py
+++ phrase.py
@@ -18,4 +18,18 @@ class Phrase:
             else:
                 display.append('_')
         print(' '.join(display))       
+      
+    def check_guess(self, guess):
+      if guess in self.phrase:
+        return True
+      
+      else:
+        return False
+    
+    def check_complete(self, guesses):
+      for letter in self.phrase:
+        if letter not in guesses:
+          return False
+        
+      return True  
         
